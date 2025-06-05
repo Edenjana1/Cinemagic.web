7ca15b2f604f91ccd3f184ee40e44d764a5b1796
Edenjana1 <edenjanashvili@gmail.com>
2025-06-05 21:11:52 +0300

    הוספתי קישורים לסרטים כך שאחרי קנייה יכלו לצפות בסרט

--- Cinemagic/Pages/Purchases/WatchPurchase.cshtml.cs
+++ Cinemagic/Pages/Purchases/WatchPurchase.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
+using static System.Net.WebRequestMethods;
 
 namespace Cinemagic.Pages.Purchases
 {
@@ -26,7 +27,8 @@ namespace Cinemagic.Pages.Purchases
         // מילון כתובות URL לסרטים
         public Dictionary<int, string> MovieUrls = new Dictionary<int, string>()
         {
-            { 1, "https://hdtodayz.to/movie/watch-wicked-hd-117565" },
+            { 1, "https://www6.f2movies.to/movie/wicked-117565" },
+            { 2, "https://www6.f2movies.to/movie/dune-6752" },
             
             // הוסף כאן את כל כתובות ה-URL של הסרטים
         };
@@ -34,21 +36,33 @@ namespace Cinemagic.Pages.Purchases
         // מילון כתובות URL לסדרות
         public Dictionary<int, string> SeriesUrls = new Dictionary<int, string>()
         {
-            
+            { 1, "https://www6.f2movies.to/tv/game-of-thrones-39539" },
+            { 2, "https://www6.f2movies.to/tv/cobra-kai-38555" },
             // הוסף כאן את כל כתובות ה-URL של הסדרות
         };
 
         public async Task OnGetAsync()
         {
+            var userIdStr = HttpContext.Session.GetString("UserId");
+            if (string.IsNullOrEmpty(userIdStr) || !int.TryParse(userIdStr, out int userId))
+            {
+                // אפשר להפנות לדף התחברות או להציג הודעה שאין משתמש
+                MoviePurchases = new List<Purchase>();
+                SeriesPurchases = new List<Purchase>();
+                return;
+            }
+
+            // שליפת רכישות לפי המשתמש שמחובר בפועל
             MoviePurchases = await _context.Purchases
                 .Include(p => p.Movies)
-                .Where(p => p.MemberID == UserId && p.MovieID != null)
+                .Where(p => p.MemberID == userId && p.MovieID != null)
                 .ToListAsync();
 
             SeriesPurchases = await _context.Purchases
                 .Include(p => p.Series)
-                .Where(p => p.MemberID == UserId && p.SerieID != null)
+                .Where(p => p.MemberID == userId && p.SerieID != null)
                 .ToListAsync();
         }
+
     }
 }
